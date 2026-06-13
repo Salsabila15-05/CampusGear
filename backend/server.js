0e9ed42d9b2b8c6add373805b5da4dfeb90b7cb2
Jihan Salsabila <jihansalsabila512@gmail.com>
2026-06-13 12:57:45 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.get('/', (req, res) => {
   res.send('Server CampusGear Berjalan');
 });
 
-app.listen(3000, () => {
-  console.log('Server berjalan di port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server berjalan di port ' + PORT);
 });
